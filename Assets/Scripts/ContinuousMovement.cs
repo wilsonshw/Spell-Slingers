ac0af5b3b20390088e1d497570f76e59e78d9a0e
Wilson <wilsonshw83@gmail.com>
2021-04-15 02:25:30 +0800

    Added joystick movement
    Left joystick movement part1

---
+++ Assets/Scripts/ContinuousMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+public class ContinuousMovement : MonoBehaviour
+{
+    public float speed = 1;
+    public XRNode inputSource;
+    private XRRig rig;
+    private Vector2 inputAxis;
+    private CharacterController character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource); //left controller (see inspector)
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis); //analog stick
+    }
+
+    private void FixedUpdate()
+    {
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0); //head facing angle, about y-axis (vertical)
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y); //multiplying quaternion by the vector rotates the vector by that much angle
+        character.Move(direction * Time.fixedDeltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class LocomotionController : MonoBehaviour
+{
+    public XRController leftTeleRay;
+    public XRController rightTeleRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(leftTeleRay)
+        {
+            leftTeleRay.gameObject.SetActive(CheckIfActivated(leftTeleRay));
+        }
+
+        if (rightTeleRay)
+        {
+            rightTeleRay.gameObject.SetActive(CheckIfActivated(rightTeleRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold); //if trigger pressed beyond threshold, return true
+        return isActivated;
+    }
+}

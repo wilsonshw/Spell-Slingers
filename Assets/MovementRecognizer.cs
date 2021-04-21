fa67ae2919052a98e817d7949faf15aa44317b7d
Wilson <wilsonshw83@gmail.com>
2021-04-21 22:42:28 +0800

    Gesture Recognition
    Implementation and some Blender model imports

---
+++ Assets/MovementRecognizer.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using PDollarGestureRecognizer;
+using System.IO;
+
+using UnityEngine.Events;
+
+public class MovementRecognizer : MonoBehaviour
+{
+
+    public XRNode inputSource;
+    public InputHelpers.Button inputButton;
+    public float inputThreshold = 0.1f;
+    public Transform movementSource;
+
+    public float newPosThresholdDistance = 0.05f;
+    public GameObject debugCubePrefab;
+    public bool creationMode = true; //create or recognize gesture
+    public string newGestureName; //if in create mode
+
+    public float recognitionThreshold = 0.9f;
+
+    [System.Serializable]
+    public class UnityStringEvent : UnityEvent<string> { }
+    public UnityStringEvent OnRecognized;
+
+    private List<Gesture> trainingSet = new List<Gesture>();
+    private bool isMoving = false;
+    private List<Vector3> positionsList = new List<Vector3>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] gestureFiles = Directory.GetFiles(Application.persistentDataPath, "*.xml");
+        foreach (var item in gestureFiles)
+        {
+            trainingSet.Add(GestureIO.ReadGestureFromFile(item));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputHelpers.IsPressed(InputDevices.GetDeviceAtXRNode(inputSource), inputButton, out bool isPressed, inputThreshold);
+
+        //start movement
+        if(!isMoving && isPressed)
+        {
+            StartMovement();
+        }
+        else if(isMoving && !isPressed) //ending movement
+        {
+            EndMovement();
+        }
+        else if(isMoving && isPressed) //updating movement
+        {
+            UpdateMovement();
+        }
+    }
+
+    void StartMovement()
+    {
+        isMoving = true;
+        positionsList.Clear();
+        positionsList.Add(movementSource.position);
+
+        if(debugCubePrefab)
+            Destroy(Instantiate(debugCubePrefab, movementSource.position, Quaternion.identity), 3);
+    }
+
+    void EndMovement()
+    {
+        isMoving = false;
+
+        //Create gesture from position list
+        Point[] pointArray = new Point[positionsList.Count];
+
+        for (int i = 0;i<positionsList.Count;i++)
+        {
+            Vector2 screenPoint = Camera.main.WorldToScreenPoint(positionsList[i]);
+            pointArray[i] = new Point(screenPoint.x, screenPoint.y, 0);
+        }
+
+        Gesture newGesture = new Gesture(pointArray);
+
+        //Add new gesture to training set
+        if(creationMode)
+        {
+            newGesture.Name = newGestureName;
+            trainingSet.Add(newGesture);
+
+            string fileName = Application.persistentDataPath + "/" + newGestureName + ".xml";
+            GestureIO.WriteGesture(pointArray, newGestureName, fileName);
+        }
+        else
+        {
+            Result result = PointCloudRecognizer.Classify(newGesture, trainingSet.ToArray());
+            Debug.Log(result.GestureClass + result.Score);
+            if (result.Score > recognitionThreshold)
+            {
+                OnRecognized.Invoke(result.GestureClass);
+            }
+
+        }
+    }
+
+    void UpdateMovement()
+    {
+        Vector3 lastPosition = positionsList[positionsList.Count - 1];
+        if (Vector3.Distance(movementSource.position, lastPosition) > newPosThresholdDistance)
+        {
+            positionsList.Add(movementSource.position);
+            if (debugCubePrefab)
+                Destroy(Instantiate(debugCubePrefab, movementSource.position, Quaternion.identity), 3);
+        }
+    }
+}
---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public List<GameObject> objects;
+    // Start is called before the first frame update
+    public void Spawn(string objectName)
+    {
+        foreach (var item in objects)
+        {
+            item.SetActive(objectName == item.name);
+        }
+    }
+}
